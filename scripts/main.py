452720f63923a34c528d5a1e4962f7131b9acb66
denhateu <danilaksenow101@gmail.com>
2025-02-02 23:11:05 +0500

    feat: added start program using command line arguments

--- scripts/main.py
+++ scripts/main.py
@@ -5,37 +5,79 @@ Author: Danil Aksenow (@denhateu)
 Version: 0.0.0-alpha
 """
 
+import sys
+
 from emptyproject import EmptyProject
 from functions import show_message
 
 
+def get_lang(language):
+  try:
+    if language == "python" or int(language) == 0:
+      result = "python"
+    else:
+      return False
+  except ValueError:
+    return False
+
+  return result
+
+
 def main():
-  project_name = input("Enter project name: ")
-  author = input("Enter project author: ")
-  language = ""
+  if len(sys.argv) <= 1:
+    # No arguments
+    project_name = input("Enter project name: ")
+    author = input("Enter project author: ")
+    language = ""
 
-  # Gets project language
-  while True:
-    print()
-    print("[ 0 ] Python")
-    print()
+    # Gets project language
+    while True:
+      print()
+      print("[ 0 ] Python")
+      print()
 
-    language_number = input("Select project language: ")
-    try:
-      if int(language_number) == 0:
-        # Selected python
-        language = "python"
+      language_number = input("Select project language: ")
+
+      language = get_lang(language_number)
+      if language is not False:
+        break
       else:
-        # Language not found
-        show_message("Language not found!")
+        show_message("Select language number!")
         continue
-    except ValueError:
-      # Error
-      show_message("Please select number!")
-      continue
+  else:
+    # Arguments found, parse
 
-    break
+    # List for errors
+    errors = []
 
+    # Get project name
+    try:
+      project_name = sys.argv[1]
+    except IndexError:
+      errors.append("Input project name!")
+
+    # Get author name
+    try:
+      author = sys.argv[2]
+    except IndexError:
+      errors.append("Where project author name blyat?")
+
+    # Get project language
+    try:
+      language = get_lang(sys.argv[3])
+      if language is False:
+        errors.append("Choose correct language!")
+    except IndexError:
+      errors.append("Enter project language uebok")
+
+    if len(errors) >= 1:
+      # No errors
+      show_message(errors[0])
+      print()
+      
+      sys.exit(1)
+
+  # Create project
   empty_project = EmptyProject(project_name, author, language)
   empty_project.create()
 
